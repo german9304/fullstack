88598c5bcfe36124967f07b74b858d7314c1e595
German Razo Rodriguez <ger9366@gmail.com>
2019-10-23 13:08:54 -0700

    init files

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Welcome to full stack application</h1>
+    </div>
+  );
+}
+
+export default Home;
