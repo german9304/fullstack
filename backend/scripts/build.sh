e9d8bea243eede7d0dc1dc22549909c07cc69460
German Razo Rodriguez <ger9366@gmail.com>
2019-10-25 21:47:02 -0700

    remove sudo

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
 # cd into backend
-cd backend
-
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-cd /usr/local/bin/
+docker-compose --version
