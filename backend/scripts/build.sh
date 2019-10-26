cc85a5c82d325be420b320084cc854b6ba3a663c
German Razo Rodriguez <ger9366@gmail.com>
2019-10-26 09:48:25 -0700

    docker-compose

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -3,6 +3,8 @@
 # cd into backend
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+# ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # docker-compose --version
